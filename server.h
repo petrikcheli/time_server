a16f755b864cfc580cecba567bf4077f35dafe4f
Petr Artemyev <petricheli@mail.ru>
2024-07-02 17:08:40 +0500

    vps

--- server.h
+++ server.h
@@ -11,7 +11,7 @@
 class Server
 {
     int PORT_SERVER = 8080;
-    const char* IP_SERVER = "192.168.31.142";
+    const char* IP_SERVER = "217.144.103.236";
 
     struct sockaddr_in server_addr;
     struct sockaddr_in client_addr;
